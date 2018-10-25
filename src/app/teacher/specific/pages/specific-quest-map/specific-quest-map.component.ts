0639b6ccf8bcb3756da438658311f4827938d526
crucibles <alvaro_cedric@yahoo.com>
2018-10-26 06:01:33 +0800

    Fixed vulnerabilities

--- src/app/teacher/specific/pages/specific-quest-map/specific-quest-map.component.ts
+++ src/app/teacher/specific/pages/specific-quest-map/specific-quest-map.component.ts
@@ -410,10 +410,12 @@ export class SpecificQuestMapComponent implements OnInit, AfterViewInit {
 		var HTMLchart = document.getElementById("quest-map");
 		var ctx = (<HTMLCanvasElement>HTMLchart).getContext("2d");
 
-		this.chart = new Chart(ctx, {
+		let cc:any = {
 			data: QM,
 			options: options
-		});
+		};
+		
+		this.chart = new Chart(ctx, cc);
 	}
 
 
